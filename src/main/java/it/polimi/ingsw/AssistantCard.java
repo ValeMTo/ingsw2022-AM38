435bea7c496a3ab2d12e46393b727364b54ef149
fra-dev-1 <francesco.barisani@mail.polimi.it>
2022-04-09 10:45:31 +0200

    AssistantCard updated with Tests and changed compiler version in pom

--- src/main/java/it/polimi/ingsw/AssistantCard.java
+++ src/main/java/it/polimi/ingsw/AssistantCard.java
@@ -1,15 +1,15 @@
 package it.polimi.ingsw;
 
 public class AssistantCard {
-    private boolean used;
     final int priority;
     final int motherNatureSteps;
+    private boolean used;
 
     /**
      * AssistantCard constructor builds the card given its turn priority and motherNatureSteps parameter
      *
      * @param motherNatureSteps states the maximum amount of steps MotherNature can make if that card is played
-     * @param priority is the number used to set the order/priority of the players in a turn
+     * @param priority  is the number used to set the order/priority of the players in a turn
      */
 
     public AssistantCard(int priority, int motherNatureSteps) {
@@ -21,7 +21,6 @@ public class AssistantCard {
     /**
      * returns true if the card has already been used (or if it is being used in the current round)
      * returns false if it is still in the deck, and it hasn't been used
-     *
      */
 
     public boolean isUsed() {
@@ -30,17 +29,19 @@ public class AssistantCard {
 
     /**
      * changes the status of an unused card to used
-     *
      */
 
     public void use() {
         if (!used)
             this.used = true;
+    /*    else
+    *       throw AlreadyUsedCardException;
+    */
     }
+
 
     /**
      * returns the priority parameter of an assistant card
-     * 
      * @return value "priority" of the given Assistant card
      */
 
@@ -51,14 +52,12 @@ public class AssistantCard {
 
     /**
      * returns the "motherNatureSteps" parameter of an assistant card
-     *
      * @return the value of "motherNatureSteps" of the given Assistant card
      */
 
     public int getSteps() {
         return this.motherNatureSteps;
     }
-
 
 
 }
