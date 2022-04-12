4716076a15bfd90e145465ef8dcd29417df2ad76
fra-dev-1 <francesco.barisani@mail.polimi.it>
2022-04-12 22:42:37 +0200

    Cloud class updated and Tests completed

--- src/main/java/it/polimi/ingsw/Cloud.java
+++ src/main/java/it/polimi/ingsw/Cloud.java
@@ -1,12 +1,11 @@
-
 package it.polimi.ingsw;
 
 import java.util.HashMap;
 
 
 public class Cloud {
-    private HashMap<Color,Integer> counter;
     final int studentLimit;
+    private final HashMap<Color, Integer> counter;
 
 
     /**
@@ -19,8 +18,8 @@ public class Cloud {
     public Cloud(int studentLimit) {
         this.studentLimit = studentLimit;
         this.counter = new HashMap<Color, Integer>();
-        for(Color col : Color.values()) {
-            counter.put(col,0);
+        for (Color col : Color.values()) {
+            counter.put(col, 0);
         }
     }
 
@@ -28,6 +27,7 @@ public class Cloud {
     /**
      * Checks whether the selected cloud is full.
      * Returns true if the cloud contains the maximum amount of students (studentLimit); false otherwise
+     *
      * @return the fill status of the cloud
      */
     public boolean isFull() {
@@ -35,15 +35,13 @@ public class Cloud {
         for (Integer amount : counter.values()) {
             totalStud = totalStud + amount;
         }
-        if(totalStud == studentLimit)
-            return true;
-
-        return false;
+        return totalStud == studentLimit;
     }
 
     /**
      * Checks whether the selected cloud is empty.
      * Returns true if the cloud has no students left (zero students); false otherwise
+     *
      * @return the emptiness status of the cloud
      */
     public boolean isEmpty() {
@@ -57,12 +55,13 @@ public class Cloud {
 
     /**
      * Empties the given cloud, removing all the students it contains.
+     *
      * @return the outcome of the emptying : false is the cloud is already empty and cannot be emptied; true otherwise.
      */
     public boolean emptyCloud() {
-        if(this.isEmpty())
+        if (this.isEmpty())
             return false;
-        counter.replaceAll((key,value)->0);
+        counter.replaceAll((key, value) -> 0);
         return true;
     }
 
@@ -96,8 +95,11 @@ public class Cloud {
      * @param studentColor is the color of the student to remove from the cloud.
      * @return outcome of the removal
      */
-    public boolean removeStudent(Color studentColor) {
+    public boolean removeStudent(Color studentColor) throws NullPointerException {
+        if (studentColor == null) throw new NullPointerException();
+
         Integer previousNumStudents = counter.get(studentColor);
+
         if (previousNumStudents == null || this.countStudentByColor(studentColor) <= 0)
             return false;
         counter.put(studentColor, previousNumStudents - 1);
@@ -107,6 +109,7 @@ public class Cloud {
 
     /**
      * returns the current  amount of students of a certain color in the cloud.
+     *
      * @param color : the color of the students to be counted
      * @return the number of students of that given color that are currently contained in the cloud.
      */
