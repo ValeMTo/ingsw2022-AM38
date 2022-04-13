c53bfdf2e04347a423665ad7ba27aa356b2e90fd
Valeria <valeria.amato@mail.polimi.it>
2022-04-13 12:47:50 +0200

    Test finished for herbalist and general special Card

---
+++ src/test/java/it/polimi/ingsw/SpecialCardTest.java
@@ -0,0 +1,62 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SpecialCardTest {
+
+    private final SpecialCard priest = new SpecialCard(SpecialCardName.PRIEST);
+    private final SpecialCard herbalist = new SpecialCard(SpecialCardName.HERBALIST);
+    private final SpecialCard gambler = new SpecialCard(SpecialCardName.GAMBLER);
+
+    /**
+     * Test the increase of price.
+     * The price gets higher only for the first use.
+     */
+    @Test
+    public void increasePriceTest() {
+        assertEquals(1, priest.getCostCoin());
+        priest.use();
+        assertEquals(2, priest.getCostCoin());
+        priest.use();
+        assertEquals(2, priest.getCostCoin());
+
+        assertEquals(2, herbalist.getCostCoin());
+        herbalist.use();
+        assertEquals(3, herbalist.getCostCoin());
+        herbalist.use();
+        assertEquals(3, herbalist.getCostCoin());
+
+        assertEquals(3, gambler.getCostCoin());
+        gambler.use();
+        assertEquals(4, gambler.getCostCoin());
+        gambler.use();
+        assertEquals(4, gambler.getCostCoin());
+    }
+
+    /**
+     * Test for empty Tiles methods for no herbalist cards.
+     */
+    @Test
+    public void herbalistExceptionMethodTest() {
+        assertThrows(NotCallableMethodException.class, () -> priest.returnTile(), "Not usable method for this card: ");
+        assertThrows(NotCallableMethodException.class, () -> priest.removeTile(), "Not usable method for this card: ");
+        assertThrows(NotCallableMethodException.class, () -> priest.getTile(), "Not usable method for this card: ");
+        assertThrows(NotCallableMethodException.class, () -> priest.isEmptyOfTiles(), "Not usable method for this card: ");
+    }
+
+    /**
+     * Test empty students methods for no students special cards.
+     */
+    @Test
+    public void studentsExceptionMethodTest() {
+        assertThrows(NotCallableMethodException.class, () -> priest.addStudent(Color.BLUE), "Not usable method for this card: ");
+        assertThrows(NotCallableMethodException.class, () -> priest.removeStudent(Color.BLUE), "Not usable method for this card: ");
+        assertThrows(NotCallableMethodException.class, () -> priest.countStudents(Color.BLUE), "Not usable method for this card: ");
+        assertThrows(NotCallableMethodException.class, () -> priest.countStudents(), "Not usable method for this card: ");
+    }
+
+
+}
